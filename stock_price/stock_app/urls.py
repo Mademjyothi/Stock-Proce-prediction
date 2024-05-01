a46ee869bac47c4e23257a6253b6ab70ee77d00f
Mademjyothi <114082237+Mademjyothi@users.noreply.github.com>
2024-05-01 10:00:53 +0530

    Add files via upload

---
+++ stock_price/stock_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path,include
+from  stock_app import views
+
+urlpatterns=[
+    path('',views.index,name='ind'),
+    path('log/',views.login_user,name="log"),
+    path('reg/',views.register,name='reg'),
+    path('logout/',views.logout_user,name='logout'),
+    path('predict/<str:company>/',views.predict,name="predict"),
+]
